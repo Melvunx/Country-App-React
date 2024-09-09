bd40215af01ed4db49e8c1e62ecd374219c0e036
Melvunx <augustine.melv@gmmail.com>
2024-09-10 01:19:44 +0200

    country bar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navigation from "../components/Navigation";
 import Logo from "../components/Logo";
-import Countries from "./Countries";
+import Countries from "../components/Countries";
 
 const Home = () => {
   return (
